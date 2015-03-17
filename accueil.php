411051f095516b43df04ee85042738321d11ee5f
mlc64 <m.leclech@sfr.fr>
2015-03-17 16:47:49 +0100

    base

---
+++ accueil.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Accueil</title>
+	</head>
+	<body>
+		<header>
+
+		</header>
+
+		<div>
+
+			<label for="login"> Entrez votre login </label>
+			<input type="text" id="login" name="username"/>
+			<label for="size">Taille de l'image</label>
+			<select name="sizeAvatar" id="size">
+				<option value="size1"> Size 1</option>
+				<option value="size2"> Size 2</option>
+				<option value="size3"> Size 3</option>
+				<option value="size4"> Size 4</option>
+			</select>
+			<br/>
+			<a href='./informations.php'>Configuration</a>
+			<br/>
+			<a href='./../ProjetLaravel/public/'>Aller sur l'application</a>
+		</div>
+
+		<footer>
+
+		</footer>
+	</body>
+</html>
---
+++ informations.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Configuration</title>
+	</head>
+	<body>
+		<header>
+
+		</header>
+		
+		<div>
+			<table>
+				<tr>
+					<th>Version de l'API</th>
+					<td>??</td>
+				</tr>
+				<tr>
+					<th>Tailles d'avatar disponibles</th>
+					<td>??</td>
+				</tr>
+				<tr>
+					<th>Taille d'avatar par défaut</th>
+					<td>??</td>
+				</tr>
+				<tr>
+					<th>Formats images supportés</th>
+					<td>??</td>
+				</tr>
+			</table>
+
+			<br/>
+			<a href='./accueil.php'>Revenir à l'accueil</a>
+		</div>
+
+		<footer>
+
+		</footer>
+	</body>
+</html>
